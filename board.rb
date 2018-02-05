dd3089702bad4359a98fd4a95161f91027e3b437
James Gayfer <gayfer.james@gmail.com>
2018-02-05 14:38:42 -0800

    Add functionality to display a basic game board

--- board.rb
+++ board.rb
@@ -1,13 +1,48 @@
 class Board
 
-    def initialize(size)
-        @num_cells = size ** 2
+    def initialize(dimension)
+        @dimension = dimension
+        @num_cells = dimension ** 2
         @current_pos = 0
         @snakes_ladders = Array.new(@num_cells)
 
-        num_snakes = num_ladders = (size / 2).ceil 
+        num_snakes = num_ladders = (dimension / 2).ceil 
         generate_snakes(num_snakes)
         generate_ladders(num_ladders)
+    end
+
+    def display_board
+        (0...@dimension).reverse_each do |n|
+            start_num = (n * @dimension) + 1
+            end_num = (n * @dimension) + @dimension
+  
+            if n % 2 == 0
+                row_numbers = Array (start_num..end_num)
+            else
+                row_numbers = (Array (start_num..end_num)).reverse
+            end
+            
+            print_dashed_line
+            print_row(row_numbers)
+        end
+        print_dashed_line
+    end
+
+    private
+    def print_row(row_numbers)
+        row_numbers.each do |num|
+            padded_num_str = num.to_s.ljust(3, ' ')
+            print "|#{padded_num_str}"
+        end
+        puts '|'
+    end
+
+    private
+    def print_dashed_line
+        (@dimension * 4).times do
+            print '-'
+        end
+        puts '-'
     end
 
     private
@@ -27,5 +62,4 @@ class Board
             @snakes_ladders[start_cell] = end_cell
         end
     end
-
 end
