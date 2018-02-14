405f9d45b25c3be37b027f99255966a8fb3fdf23
James Gayfer <gayfer.james@gmail.com>
2018-02-14 14:10:16 -0800

    Remove logic to display board
    The Board shouldn't be responsible for displaying itself; something else
    should be handling the display logic. In order to display the board,
    more information is needed than what the Board class is responsible for.
    The logic for displaying the board will be broken out into its own
    class.

--- lib/board.rb
+++ lib/board.rb
@@ -10,11 +10,6 @@ class Board
     generate_ladders(num_ladders)
   end
 
-  def display(player_position_hash)
-    print_cells(player_position_hash)
-    print_ladders
-    print_snakes
-  end
 
   def end_of_board
     @num_cells - 1
@@ -40,66 +35,6 @@ class Board
 
   private
 
-  def get_row_nums(n)
-    start_num = (n * @dimension) + 1
-    end_num = (n * @dimension) + @dimension
-    num_array = Array(start_num..end_num)
-
-    return num_array.reverse unless n.even?
-    num_array
-  end
-
-  def print_cells(player_position_hash)
-    (0...@dimension).reverse_each do |n|
-      print_dashed_line
-      row_nums = get_row_nums(n)
-      print_row(row_nums, player_position_hash)
-    end
-    print_dashed_line
-    puts
-  end
-
-  def print_ladders
-    puts 'Ladders'
-    @ladders.each_with_index do |end_cell, start_cell|
-      puts "#{start_cell + 1} --> #{end_cell + 1}" if end_cell
-    end
-    puts
-  end
-
-  def print_snakes
-    puts 'Snakes'
-    @snakes.each_with_index do |end_cell, start_cell|
-      puts "#{start_cell + 1} --> #{end_cell + 1}" if end_cell
-    end
-    puts
-  end
-
-  def print_row(row_numbers, player_position_hash)
-    row_numbers.each do |num|
-      print "|#{format_cell(num, player_position_hash)}"
-    end
-    puts '|'
-  end
-
-  def print_dashed_line
-    (@dimension * 4).times do
-      print '-'
-    end
-    puts '-'
-  end
-
-  def format_cell(cell_num, player_position_hash)
-    cell_index = cell_num - 1
-    players_at_this_index =
-      player_position_hash.select { |_, v| v == cell_index }.keys
-
-    if !players_at_this_index.empty?
-      players_at_this_index.map(&:symbol).join('').ljust(3, ' ')
-    else
-      cell_num.to_s.ljust(3, ' ')
-    end
-  end
 
   def generate_snakes(num_snakes)
     num_snakes.times do
