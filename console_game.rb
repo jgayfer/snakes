8f94ec120efc4c427d3788dd0e175ec02ba9b662
James Gayfer <gayfer.james@gmail.com>
2018-02-19 09:15:50 -0800

    Update console game to display board
    By displaying the board after each turn, players can now visualize where
    they are on the board. Previously, players could not see their location;
    they just rolled the dice until someone reached the invisible finish
    line.

--- console_game.rb
+++ console_game.rb
@@ -4,14 +4,17 @@ require_relative 'lib/player'
 require_relative 'lib/player_state'
 require_relative 'lib/transition'
 require_relative 'lib/rules/standard_rules'
+require_relative 'lib/console_printer'
 
 players = [Player.new('James'), Player.new('Sebastian')]
 player_states = players.map { |player| PlayerState.new(player) }
-board = Board.new([], 10)
+board = Board.new([Transition.new(5, 50), Transition.new(87, 25)], 10)
 rules = StandardRules.new
 g = Game.new(board, player_states, rules)
+p = ConsolePrinter.new(g)
 
 until g.last_move_was_a_win
+  p.display_board
   print "It's your turn #{g.next_player}! Press any key to roll the dice: "
   gets.chomp
   move = rand(1..6)
@@ -19,4 +22,5 @@ until g.last_move_was_a_win
   g.move_next_player(move)
 end
 
+p.display_board
 puts "#{g.previous_player} wins!"
