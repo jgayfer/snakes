c8f8aa9a534a5f29de43befeb39a6befa3e1bd47
James Gayfer <gayfer.james@gmail.com>
2018-02-21 10:58:38 -0800

    Add Transition spec

---
+++ spec/transition_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper.rb'
+require_relative '../lib/snakes'
+
+RSpec.describe Snakes::Transition do
+  let(:start_num) { 24 }
+  let(:end_num) { 42 }
+  let(:transition) { Snakes::Transition.new(start_num, end_num) }
+
+  describe '#initialize' do
+    subject { transition }
+    it { is_expected.to be_a Snakes::Transition }
+  end
+end
