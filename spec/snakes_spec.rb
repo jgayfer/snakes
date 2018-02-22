92ec4f07849654a28b2d9d3764f87d78dae1ca3e
James Gayfer <gayfer.james@gmail.com>
2018-02-22 14:06:07 -0800

    Add Snakes spec

---
+++ spec/snakes_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/snakes'
+require_relative 'spec_helper'
+
+RSpec.describe Snakes do
+  let(:player_names) { %w['James Sebastian'] }
+  let(:die_sides) { 1 }
+
+  describe '.standard_game' do
+    context 'without die sides' do
+      subject { Snakes.standard_game(player_names) }
+      it { is_expected.to be_a Snakes::Game }
+    end
+
+    context 'with die sides' do
+      subject { Snakes.standard_game(player_names, die_sides) }
+      it { is_expected.to be_a Snakes::Game }
+    end
+  end
+end
